0a280cfc5439774466623d3e28e98f77b6754a9e
error25 <autovincelynch@gmail.com>
2016-05-23 20:43:56 +0100

    .

---
+++ includes/resources/query.php
@@ -0,0 +1,30 @@
+<?php
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+// Create connection
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, productname, price FROM StoreProducts";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["productname"]. " " . $row["price"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php 
 $basedir = realpath(__DIR__);
 /////include($basedir . '/includes/resources/connectDB.php');
-include($basedir . '/includes/resources/InsertProductIntoDB.php');
+//include($basedir . '/includes/resources/InsertProductIntoDB.php');
 
 
-InsertProduct("Cat: Henry", "20");
+//InsertProduct("Cat: Henry", "20");
+
+include($basedir . '/includes/resources/query.php');
 
 ?>
