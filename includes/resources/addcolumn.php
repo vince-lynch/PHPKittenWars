48aa8f5f0520c1cccef7a872bb067fbfbf10d117
error25 <autovincelynch@gmail.com>
2016-05-23 21:33:06 +0100

    .

--- includes/resources/addcolumn.php
+++ includes/resources/addcolumn.php
@@ -3,7 +3,7 @@
 include('connectDB.php');
 
 $conn = new mysqli($hostname, $username, $password, $database);
-$query = 'ALTER TABLE StoreProducts ADD ' . 'catpicture' . ' VARCHAR(30) NOT NULL';
+$query = 'ALTER TABLE StoreProducts ADD ' . 'score' . ' INT(6) NOT NULL DEFAULT 0;';
 if ($conn->query($query) === TRUE) {
     echo "New column successfully";
 } else {
