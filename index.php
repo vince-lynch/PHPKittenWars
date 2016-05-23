0599c050bf303ef140a68f339a9d3f8f3cf41f77
error25 <autovincelynch@gmail.com>
2016-05-23 19:27:13 +0100

    connecting to mySQL on heroku

---
+++ index.php
@@ -0,0 +1,16 @@
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+// Create connection
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connection was successfully established!";
