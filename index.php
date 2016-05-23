18dd4e048935a7adf1d99913855f5efa605aa4cc
error25 <autovincelynch@gmail.com>
2016-05-23 19:34:08 +0100

    creating product table

--- index.php
+++ index.php
@@ -15,4 +15,20 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connection was successfully established!";
+
+$sql = "CREATE TABLE StoreProducts (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+productname VARCHAR(30) NOT NULL,
+price INT(6) NOT NULL,
+stock VARCHAR(50),
+date_added TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Product table created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
 ?>
