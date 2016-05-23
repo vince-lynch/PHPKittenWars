294bc26966e1677dc615463693436cbffd508897
error25 <autovincelynch@gmail.com>
2016-05-23 19:46:54 +0100

    inserting cat into database

---
+++ includes/resources/InsertProductIntoDB.php
@@ -0,0 +1,15 @@
+<?php
+function InsertProduct($productName,$price,$stock){
+  $insert = "INSERT INTO StoreProducts (productname, price, stock)
+  VALUES ($productName, $price, $stock )";
+
+  if ($conn->query($insert) === TRUE) {
+      echo "New record created successfully";
+  } else {
+      echo "Error: " . $insert . "<br>" . $conn->error;
+  }
+
+  $conn->close();
+}
+
+?>
--- index.php
+++ index.php
@@ -1,34 +1,8 @@
 <?php 
-$url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
 
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
+include '/includes/resources/connectDB.php';
+include '/includes/resources/InsertProductIntoDB.php';
 
-// Create connection
-$conn = new mysqli($hostname, $username, $password, $database);
+InsertProduct("Cat: Anton",99,"1")
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connection was successfully established!";
-
-$sql = "CREATE TABLE StoreProducts (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-productname VARCHAR(30) NOT NULL,
-price INT(6) NOT NULL,
-stock VARCHAR(50),
-date_added TIMESTAMP
-)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Product table created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
-$conn->close();
 ?>
