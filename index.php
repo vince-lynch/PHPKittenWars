d73f172820235b58e72ce0b834b6e0771cf022d3
error25 <autovincelynch@gmail.com>
2016-05-23 19:54:43 +0100

    .

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
-
-require('../includes/resources/connectDB.php');
-require('../includes/resources/InsertProductIntoDB.php');
+include $_SERVER['DOCUMENT_ROOT'] . 'includes/resources/connectDB.php';
+include $_SERVER['DOCUMENT_ROOT'] . 'includes/resources/InsertProductIntoDB.php';
+//require('../includes/resources/connectDB.php');
+//require('../includes/resources/InsertProductIntoDB.php');
 
 InsertProduct("Cat: Anton",99,"1")
 
