b602ec81c3afd457c6df23f80d85237b4cdf19fa
error25 <autovincelynch@gmail.com>
2016-05-23 21:17:42 +0100

    .

--- includes/resources/query.php
+++ includes/resources/query.php
@@ -15,7 +15,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["productname"]. " " . $row["imageurl"];
+        echo "id: " . $row["id"]. " - Name: " . $row["productname"]. " " . $row["catpicture"];
     }
 } else {
     echo "0 results";
