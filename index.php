660d2ef08ea56e963972b09d1a1e4a46bcd738eb
error25 <autovincelynch@gmail.com>
2016-05-23 21:02:22 +0100

    .

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
+<!DOCTYPE html>
+<html>
 <?php 
 $basedir = realpath(__DIR__);
 /////include($basedir . '/includes/resources/connectDB.php');
 //include($basedir . '/includes/resources/InsertProductIntoDB.php');
 
+$h1 = "kitten wars";
 
 //InsertProduct("Cat: Henry", "20");
 
 include($basedir . '/includes/resources/updaterecord.php');
 
 ?>
+<head>
+  <title></title>
+</head>
+<body>
+<h1><?php echo ' '. $h1; ?></h1>
+
+</body>
+</html>
