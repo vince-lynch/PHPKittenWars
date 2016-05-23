9d4fed2dcc832a932ce042b5a128c41ac0e5291e
error25 <autovincelynch@gmail.com>
2016-05-23 21:08:26 +0100

    .

--- includes/resources/query.php
+++ includes/resources/query.php
@@ -8,13 +8,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id, productname, price, imageurl FROM StoreProducts";
+$sql = "SELECT id, productname, imageurl FROM StoreProducts";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["productname"]. " " . $row["price"]. "<br/>" . $row["imageurl"];
+        echo "id: " . $row["id"]. " - Name: " . $row["productname"]. " " . $row["imageurl"];
     }
 } else {
     echo "0 results";
