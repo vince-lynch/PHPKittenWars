e53a589d67627bf539c513ce01f91cd99b8bc40e
error25 <autovincelynch@gmail.com>
2016-05-23 21:43:23 +0100

    .

--- addcat.php
+++ addcat.php
@@ -1,5 +1,22 @@
 <?php
 $basedir = realpath(__DIR__);
+
+$target_dir = $basedir . '/assets/images/';
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
 include($basedir . '/includes/resources/insertcat.php');
 InsertCat($_POST["catsname"],$_POST["catsphoto"]);
 //Welcome <?php echo $_POST["catsname"]; 
--- new.php
+++ new.php
@@ -7,9 +7,10 @@
 
 <h1>Add your cat</h1>
 
-<form action="addcat.php" method="post">
+<form action="addcat.php" method="post" enctype="multipart/form-data">
 Cat's name: <input type="text" name="catsname"><br>
 Cat's Photo URL: <input type="text" name="catsphoto"><br>
+<input type="file" name="fileToUpload" id="fileToUpload">
 <input type="submit">
 </form>
 
