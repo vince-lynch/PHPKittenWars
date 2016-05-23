64c96ba20d654c480358f41a1009419696e640ee
error25 <autovincelynch@gmail.com>
2016-05-23 20:51:31 +0100

    .

--- includes/resources/addcolumn.php
+++ includes/resources/addcolumn.php
@@ -4,8 +4,10 @@ include('connectDB.php');
 
 $conn = new mysqli($hostname, $username, $password, $database);
 $query = 'ALTER TABLE assesment ADD ' . 'imageurl' . ' TINYINT NOT NULL DEFAULT \'0\'';
-if($conn ->query($query)) {
-    echo "It worked";
+if ($conn->query($query) === TRUE) {
+    echo "New column successfully";
+} else {
+    echo "Error: " . $conn->error;
 }
 
 ?>
