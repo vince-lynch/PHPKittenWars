8c7404e344d4147572c898bb1473f467a6907b2b
error25 <autovincelynch@gmail.com>
2016-05-23 20:10:42 +0100

    .

--- includes/resources/InsertProductIntoDB.php
+++ includes/resources/InsertProductIntoDB.php
@@ -1,6 +1,8 @@
 <?php
 echo "loaded insertproductintodb.php";
 function InsertProduct($productName,$price,$stock){
+  echo "InsertProduct() function called";
+
   $insert = "INSERT INTO StoreProducts (productname, price, stock)
   VALUES ($productName, $price, $stock )";
 
