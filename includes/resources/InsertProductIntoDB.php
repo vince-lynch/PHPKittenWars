754c50fa609e75dca3f2d3768ae1897b82be994e
error25 <autovincelynch@gmail.com>
2016-05-23 20:25:46 +0100

    .

--- includes/resources/InsertProductIntoDB.php
+++ includes/resources/InsertProductIntoDB.php
@@ -9,8 +9,9 @@ function InsertProduct($productName,$price,$stock){
   echo "InsertProduct() function called";
   echo "{$insert}";
   echo "{$GLOBALS['url']}";
+  $connection = $GLOBALS['conn'];
 
-  if ($GLOBALS['conn']->query($insert) === TRUE) {
+  if ($connection->query($insert) === TRUE) {
       echo "New record created successfully";
   } else {
       echo "Error: " . $insert . "<br>" . $conn->error;
