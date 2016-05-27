4d9855dbfd651b1fda0c9cddba5894e4137c5a09
error25 <autovincelynch@gmail.com>
2016-05-27 19:55:45 +0100

    .

--- includes/page.php
+++ includes/page.php
@@ -5,8 +5,8 @@ echo $basedir;
 if (!class_exists('S3'))require_once($basedir . '/S3/' . 'S3.php');
  
 //AWS access info
-if (!defined('awsAccessKey')) define('awsAccessKey', 'CHANGETHIS');
-if (!defined('awsSecretKey')) define('awsSecretKey', 'CHANGETHISTOO');
+if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAIJ5NUVL5MA3QQMZQ');
+if (!defined('awsSecretKey')) define('awsSecretKey', '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');
  
 //instantiate the class
 $s3 = new S3(awsAccessKey, awsSecretKey);
