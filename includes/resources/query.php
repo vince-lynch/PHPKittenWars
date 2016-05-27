2015de7af9e73de6754fd2d588620211e58d7ac4
error25 <autovincelynch@gmail.com>
2016-05-27 20:53:04 +0100

    .

--- includes/resources/query.php
+++ includes/resources/query.php
@@ -15,7 +15,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<li class='cat'>" . "id: " . $row["id"]. " - Name: " . $row["productname"]. " " . " Score: " . $row["score"]. "<img src='/assets/images/" . $row["catpicture"] . "'/>" . "</li>";
+        echo "<li class='cat'>" . "id: " . $row["id"]. " - Name: " . $row["productname"]. " " . " Score: " . $row["score"]. "<img src='http://kittenwars.s3.amazonaws.com/" . $row["catpicture"] . "'/>" . "</li>";
     }
 } else {
     echo "0 results";
