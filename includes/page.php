cd94f70bcd6f93372f5c409b1f37528184964fcd
error25 <autovincelynch@gmail.com>
2016-05-27 19:47:10 +0100

    .

---
+++ includes/page.php
@@ -0,0 +1,16 @@
+<?php
+$basedir = realpath(__DIR__);
+echo $basedir;
+//include the S3 class              
+if (!class_exists('S3'))require_once('S3.php');
+ 
+//AWS access info
+if (!defined('awsAccessKey')) define('awsAccessKey', 'CHANGETHIS');
+if (!defined('awsSecretKey')) define('awsSecretKey', 'CHANGETHISTOO');
+ 
+//instantiate the class
+$s3 = new S3(awsAccessKey, awsSecretKey);
+ 
+//we'll continue our script from here in step 4!
+ 
+?>
