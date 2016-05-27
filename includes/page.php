3fa151060341d7a95b12ce7f1b3c09895bd4258d
error25 <autovincelynch@gmail.com>
2016-05-27 19:59:44 +0100

    .

--- includes/page.php
+++ includes/page.php
@@ -1,3 +1,5 @@
+<html>
+<body>
 <?php
 $basedir = realpath(__DIR__);
 echo $basedir;
@@ -11,6 +13,32 @@ if (!defined('awsSecretKey')) define('awsSecretKey', 'zCapWtPejDW/Xq1wzH3NQ6b7sz
 //instantiate the class
 $s3 = new S3(awsAccessKey, awsSecretKey);
  
-//we'll continue our script from here in step 4!
+
+
+//check whether a form was submitted
+if(isset($_POST['Submit'])){
  
+    //retreive post variables
+    $fileName = $_FILES['theFile']['name'];
+    $fileTempName = $_FILES['theFile']['tmp_name'];
+     
+
+
+}
+
+$s3->putBucket("jurgens-nettuts-tutorial", S3::ACL_PUBLIC_READ);<br /><br />
+ 
+//move the file
+if ($s3->putObjectFile($fileTempName, "jurgens-nettuts-tutorial", $fileName, S3::ACL_PUBLIC_READ)) {
+    echo "We successfully uploaded your file.";
+}else{
+    echo "Something went wrong while uploading your file... sorry.";
+}
 ?>
+
+<form action="" method="post" enctype="multipart/form-data">
+  <input name="theFile" type="file" />
+  <input name="Submit" type="submit" value="Upload">
+</form>
+</body>
+</html>
