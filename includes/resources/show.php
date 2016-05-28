2c68d6b1b755f889831a3cb2dbff535fbfdaa9be
error25 <autovincelynch@gmail.com>
2016-05-28 10:05:38 +0100

    .

--- includes/resources/show.php
+++ includes/resources/show.php
@@ -3,9 +3,13 @@ include('connectDB.php');
 
 $editID = 1;
 
-$query = mysql_query("SELECT * FROM StoreProducts WHERE ID = '$editID'");
-$row = mysql_fetch_array($query);
 
-print_r($row);
+$sql = "SELECT * FROM StoreProducts WHERE ID = 1";
+$result = $conn->query($sql);
+
+//$query = mysql_query("SELECT * FROM StoreProducts WHERE ID = '$editID'");
+//$row = mysql_fetch_array($query);
+
+print_r($result);
 
 ?>
