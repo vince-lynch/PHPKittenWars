ff363c9e0766e6b8fe353e2f5804fe0b90b7bb79
error25 <autovincelynch@gmail.com>
2016-05-28 12:50:15 +0100

    pushing to heroku

--- includes/resources/show.php
+++ includes/resources/show.php
@@ -9,12 +9,12 @@ print 'script got to here line - 8';
 $row = $result->fetch_array();
 print 'script got to here - line 10';
 echo 'echo row[0]' . $row[0]; // 42
-print '<br>' . $row . '</br>';
+//print '<br>' . $row . '</br>';
 print '<br>' . $row[1] . '</br>';
-print '<br>' . $row[2] . '</br>';
-print '<br>' . $row[3] . '</br>';
-print '<br>' . $row[4] . '</br>';
-print '<br>' . $row[5] . '</br>';
+//print '<br>' . $row[2] . '</br>';
+//print '<br>' . $row[3] . '</br>';
+//print '<br>' . $row[4] . '</br>';
+//print '<br>' . $row[5] . '</br>';
 print '<br>' . $row[6] . '</br>';
 print 'script got to here - line 18';
 
