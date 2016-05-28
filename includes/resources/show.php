4f0afbac6e74e052547a80c31e684949bddcba1c
error25 <autovincelynch@gmail.com>
2016-05-28 13:13:51 +0100

    made a config ru

--- includes/resources/show.php
+++ includes/resources/show.php
@@ -6,7 +6,7 @@ echo 'reached the function';
 
 echo $editID;
 
-$sql = "SELECT * FROM StoreProducts WHERE id = $editID";
+$sql = "SELECT * FROM StoreProducts WHERE id = '{$editID}'";
 $result = $conn->query($sql);
 print 'script got to here line - 8';
 $row = $result->fetch_array();
