712c833ee60a6ca7bf587ec0be5f10634460e070
error25 <autovincelynch@gmail.com>
2016-05-28 10:08:11 +0100

    .

--- includes/resources/show.php
+++ includes/resources/show.php
@@ -5,11 +5,11 @@ $editID = 1;
 
 
 $sql = "SELECT * FROM StoreProducts WHERE ID = 1";
-//$result = $conn->query($sql);
+$result = $conn->query($sql);
 
 //$query = mysql_query("SELECT * FROM StoreProducts WHERE ID = '$editID'");
 //
-$row = mysql_fetch_array($sql);
+$row = mysql_fetch_array($result);
 
 print_r($row);
 
