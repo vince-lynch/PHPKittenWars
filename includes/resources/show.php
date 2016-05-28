9f234128871a8dcf64726a1af51b62c511247456
error25 <autovincelynch@gmail.com>
2016-05-28 10:13:44 +0100

    .

--- includes/resources/show.php
+++ includes/resources/show.php
@@ -4,13 +4,25 @@ include('connectDB.php');
 $editID = 1;
 
 
-$sql = "SELECT * FROM StoreProducts WHERE ID = 1";
-$result = $conn->query($sql);
+$result = mysql_query("SELECT * FROM StoreProducts WHERE id = '1'");
+if (!$result) {
+    echo 'Could not run query: ' . mysql_error();
+    exit;
+}
+$row = mysql_fetch_row($result);
+
+echo $row[0]; // 42
+echo $row[1]; // the email value
+echo $row[2]; // the email value
+
+
+// $sql = "SELECT * FROM StoreProducts WHERE ID = 1";
+// $result = $conn->query($sql);
 
 //$query = mysql_query("SELECT * FROM StoreProducts WHERE ID = '$editID'");
 //
-$row = mysql_fetch_array($result);
+// $row = mysql_fetch_array($result);
 
-print_r($row);
+// print_r($row);
 
 ?>
