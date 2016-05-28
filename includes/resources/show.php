dabb1374bf7b7c4bc349115223ae7a061d66a077
error25 <autovincelynch@gmail.com>
2016-05-28 13:28:28 +0100

    made a config ru

--- includes/resources/show.php
+++ includes/resources/show.php
@@ -10,13 +10,13 @@ echo $editID;
 
 $sql = "SELECT * FROM StoreProducts WHERE id = '{$editID}'";
 $result = $conn->query($sql);
-print 'script got to here line - 8';
+//print 'script got to here line - 8';
 $row = $result->fetch_array();
-print 'script got to here - line 10';
-print '<br>' . $row[0] . '</br>'; // ID
-print '<br>' . $row[1] . '</br>'; //Cat Name
-print '<br>' . $row[6] . '</br>';//URL.jpg
-print 'script got to here - line 18';
+//print 'script got to here - line 10';
+//print '<br>' . $row[0] . '</br>'; // ID
+//print '<br>' . $row[1] . '</br>'; //Cat Name
+//print '<br>' . $row[6] . '</br>';//URL.jpg
+//print 'script got to here - line 18';
 $catArray = [$row[0],$row[1],$row[6]]
 }
 
