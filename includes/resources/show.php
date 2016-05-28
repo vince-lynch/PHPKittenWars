1ca0d4921fe89681494efa6764b7b2526b74ad7b
error25 <autovincelynch@gmail.com>
2016-05-28 12:44:20 +0100

    pushing to heroku

--- includes/resources/show.php
+++ includes/resources/show.php
@@ -13,6 +13,7 @@ print '<br>' . $row . '</br>';
 print '<br>' . $row[1] . '</br>';
 print '<br>' . $row[2] . '</br>';
 print '<br>' . $row[3] . '</br>';
+print '<br>' . $row[4] . '</br>';
 print 'script got to here - line 13';
 
 
